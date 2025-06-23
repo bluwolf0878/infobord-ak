c4cf32bc9547b668a72594aca3aa987348bf6d4f
andreas <andr493i@elev.techcollege.dk>
2025-06-23 08:20:52 +0200

    background

--- src/main.jsx
+++ src/main.jsx
@@ -11,40 +11,40 @@ import { Galeri } from './galeri';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <div className="grid grid-cols-9 grid-rows-10 gap-2 h-screen w-screen text-center text-lg font-semibold">
+  <div className="grid grid-cols-9 grid-rows-10 gap-2 h-screen w-screen text-center text-lg font-semibold bg-[url('public/simple-stripes-xbUVtyfBv68-unsplash 1.png)] bg-cover bg-center h-auto w-auto">
 
     {/* Top bar - Time */}
-    <div className="row-start-1 row-end-2 col-start-1 col-end-10 bg-blue-600 p-4">
+    <div className="row-start-1 row-end-2 col-start-1 col-end-10 bg-[url('public/Techcollege logo Hvid 1.png')] bg-cover bg-center h-64 w-full p-4">
       <Time />
     </div>
 
     {/* Bottom bar - Nyheder */}
-    <div className="row-start-10 row-end-11 col-start-1 col-end-10 bg-red-600 p-4">
+    <div className="row-start-10 row-end-11 col-start-1 col-end-10 border-blue-400 border-2 p-4">
       <Nyheder />
     </div>
 
     {/* Top right - Menu */}
-    <div className="row-start-2 row-end-6 col-start-7 col-end-10 bg-green-600 p-4">
+    <div className="row-start-2 row-end-6 col-start-7 col-end-10 border-blue-300 border-2 p-4">
       <Menu />
     </div>
 
     {/* Bottom right - Vejr */}
-    <div className="row-start-6 row-end-10 col-start-7 col-end-10 bg-yellow-500 text-black p-4">
+    <div className="row-start-6 row-end-10 col-start-7 col-end-10 border-blue-400 border-2 text-black p-4">
       <Vejr />
     </div>
 
     {/* Top left - ClassSchedule */}
-    <div className="row-start-2 row-end-6 col-start-1 col-end-4 bg-purple-600 p-4">
+    <div className="row-start-2 row-end-6 col-start-1 col-end-4 border-blue-400 border-2 p-4">
       <ClassSchedule />
     </div>
 
     {/* Bottom left - BusSchedule */}
-    <div className="row-start-6 row-end-10 col-start-1 col-end-4 bg-pink-500 p-4">
+    <div className="row-start-6 row-end-10 col-start-1 col-end-4 border-blue-400 border-2 p-4">
       <BusSchedule />
     </div>
 
     {/* Center - Galeri */}
-    <div className="row-start-2 row-end-10 col-start-4 col-end-7 bg-gray-700 p-4">
+    <div className="row-start-2 row-end-10 col-start-4 col-end-7 border-blue-400 border-2 p-4">
       <Galeri />
     </div>
 
